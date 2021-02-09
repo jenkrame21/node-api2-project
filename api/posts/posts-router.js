e54d3f1afa3c6667d3afaf0a1b9783b79ed0715b
Jennifer Kramer <jenkrame@gmail.com>
2021-02-09 16:46:13 -0500

    Got first GET request working. Onto 2nd GET.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,31 @@
 // implement your posts router here
+const Post = require('./posts-model');
+const express = require('express');
+
+const router = express.Router();
+
+// 1 - GET - /api/posts - Returns **an array of all the post objects** contained in the database
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ 
+                message: "The posts information could not be retrieved" 
+            });
+        });
+});
+
+// 2 - GET - /api/posts/:id - Returns **the post object with the specified id**
+
+// 3 - POST - /api/posts - Creates a post using the information sent inside the request body and returns **the newly created post object**
+
+// 4 - PUT - /api/posts/:id - Updates the post with the specified id using data from the request body and **returns the modified document**, not the original
+
+// 5 - DELETE - /api/posts/:id - Removes the post with the specified id and returns the **deleted post object**
+
+// 6 - GET - /api/posts/:id/comments - Returns an **array of all the comment objects** associated with the post with the specified id
+
+module.exports = router;
